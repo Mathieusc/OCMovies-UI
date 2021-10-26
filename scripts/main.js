2fdf95e7297c82198e4ff6c36cbfedad9951c959
Mathieusc <mathieu.schweitzer@hotmail.fr>
2021-10-26 12:16:19 +0200

    fetch api calls for all categories

--- scripts/main.js
+++ scripts/main.js
@@ -1,73 +1,98 @@
 
-// let movies = fetch("http://localhost:8000/api/v1/titles/")
-// .then(response => response.json())
-// .then(response =>
-//     console.log(JSON.stringify(response)))
-// .then(response => JSON.stringify(response))
-// .catch(error => console.log("Erreur: " + error));
-
-// console.log(movies);
-
-const topRatedMoviesApi = "http://localhost:8000/api/v1/titles/?imdb_score_min=9.3&page_size=100";
-
+const topRatedMoviesApi = "http://localhost:8000/api/v1/titles/?imdb_score_min=9.3&page_size=100&sort_by=-imdb_score";
 let topRatedMoviesCarrousel = document.getElementsByClassName("carrousel_container top-rated").item(0).getElementsByTagName("img");
-
-let bestMovie;
+let topRatedMovies;
 let bestMovieImage = document.getElementsByClassName("best_movie_image").item(0);
-console.log("Best movie image:");
-console.log(bestMovieImage);
+let bestMovie;
 
-let topRatedmoviesPageOne;
+const animeMoviesApi = "http://localhost:8000/api/v1/titles/?imdb_score_min=8.5&page_size=100&sort_by=-imdb_score&genre=Animation";
+let animeMoviesCarrousel = document.getElementsByClassName("carrousel_container anime").item(0).getElementsByTagName("img");
+let animeMovies;
+
+const horrorMoviesApi = "http://localhost:8000/api/v1/titles/?imdb_score_min=8.4&page_size=100&sort_by=-imdb_score&genre=Horror";
+let horrorMoviesCarrousel = document.getElementsByClassName("carrousel_container horror").item(0).getElementsByTagName("img");
+let horrorMovies;
+
+const musicalMoviesApi = "http://localhost:8000/api/v1/titles/?imdb_score_min=8.6&page_size=100&sort_by=-imdb_score&genre=Musical";
+let musicalMoviesCarrousel = document.getElementsByClassName("carrousel_container musical").item(0).getElementsByTagName("img");
+let musicalMovies;
+
+
 // API request, get all image urls from the top rated movies inside a new Array:
 const fetchTopRatedMovies = async() => {
-    topRatedmoviesPageOne = await fetch(topRatedMoviesApi)
+    topRatedMovies = await fetch(topRatedMoviesApi)
     .then(response => response.json())
-    // To get the results: moviesPageOne.results
-    
-    // Get the results from page 2 since we need 7 image in total (only 5 movies per page).
-    let topRatedmoviesPageTwo = topRatedmoviesPageOne.next;
-    topRatedmoviesPageTwo = await fetch(topRatedmoviesPageOne.next)
-    .then(response => response.json())
-    //testMoviesImages[0].src = movies.results[0].image_url;
     
     // Get the best movie apart from the other.
-    bestMovie = topRatedmoviesPageOne.results[0];
+    bestMovie = topRatedMovies.results[0];
     
-    // Get all image urls from the 1st page inside a new Object:
-    let topRatedMoviesImageUrl_1 = topRatedmoviesPageOne.results.map(imageUrl => {
+    // Map all image urls from the results inside a new Object:
+    let topRatedMoviesImagesUrl = topRatedMovies.results.map(imageUrl => {
         return imageUrl.image_url;
     });
-    // Get all image urls from the 2nd page inside another Object:
-    let topRatedMoviesImageUrl_2 = topRatedmoviesPageTwo.results.map(imageUrl => {
-        return imageUrl.image_url;
-    })
-    
-    console.log("Page 1 urls: ");
-    console.log(topRatedMoviesImageUrl_1);
-    console.log("Page 2 urls: ");
-    console.log(topRatedMoviesImageUrl_2);
     
     // Remove the first movie since it is the best one. The best movie is displayed on it's own.
-    topRatedMoviesImageUrl_1.shift();
+    topRatedMoviesImagesUrl.shift();
     
-    // Slice the second page urls since we only need 7 movies in total.
-    //topRatedMoviesImageUrl_2 = topRatedMoviesImageUrl_2.slice(0, 3);
-    
-    // Merge the urls from both pages into a single Array, created before the fetch call.
-    topRatedMoviesUrl = [...topRatedMoviesImageUrl_1, ...topRatedMoviesImageUrl_2];
-    console.log("Top rated movies Url : ");
-    console.log(topRatedMoviesUrl);
+    // Slice the urls since we only need 7 movies in total.
+    let topRatedMoviesImages = topRatedMoviesImagesUrl.slice(0, 7);
     
     // Insert pictures into the carrousel for the corresponding category.
-    console.log("Top rated carrousel");
-    console.log(topRatedMoviesCarrousel);
     for (let i = 0; i < topRatedMoviesCarrousel.length; i++) {
-        topRatedMoviesCarrousel[i].src = topRatedMoviesUrl[i];
-    }
-    
-    console.log("Best movie info:");
-    console.log(bestMovie);
+        topRatedMoviesCarrousel[i].src = topRatedMoviesImages[i];
+    };
 };
 
-let topRatedMoviesUrl = [];
+
+const fetchAnimeMovies = async() => {
+    animeMovies = await fetch(animeMoviesApi)
+    .then(response => response.json())
+
+    let animeMoviesImageUrl = animeMovies.results.map(imageUrl => {
+        return imageUrl.image_url;
+    });
+
+    let animeMoviesImages = animeMoviesImageUrl.slice(0, 7);
+
+    for (let i = 0; i < animeMoviesCarrousel.length; i++) {
+        animeMoviesCarrousel[i].src = animeMoviesImages[i];
+    }
+};
+
+
+const fetchHorrorMovies = async() => {
+    horrorMovies = await fetch(horrorMoviesApi)
+    .then(response => response.json())
+
+    let horrorMoviesImagesUrl = horrorMovies.results.map(imageUrl => {
+        return imageUrl.image_url;
+    });
+
+    let horrorMoviesImages = horrorMoviesImagesUrl.slice(0, 7);
+
+    for (let i=0; i<horrorMoviesCarrousel.length; i++) {
+        horrorMoviesCarrousel[i].src = horrorMoviesImages[i];
+    }
+};
+
+
+const fetchMusicalMovies = async() => {
+    musicalMovies = await fetch(musicalMoviesApi)
+    .then(response => response.json())
+
+    let musicalMoviesImagesUrl = musicalMovies.results.map(imageUrl => {
+        return imageUrl.image_url;
+    });
+
+    let musicalMoviesImages = musicalMoviesImagesUrl.slice(0, 7);
+
+    for (let i = 0; i < musicalMoviesCarrousel.length; i ++) {
+        musicalMoviesCarrousel[i].src = musicalMoviesImagesUrl[i];
+    }
+};
+
+
 fetchTopRatedMovies();
+fetchAnimeMovies();
+fetchHorrorMovies();
+fetchMusicalMovies();
