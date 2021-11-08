03c93fd2b9fc742536b0b623bc34cdeb60884123
Mathieusc <62360534+Mathieusc@users.noreply.github.com>
2021-11-08 20:57:10 +0100

    Dev (#2)
    * refacto fetch calls
    
    * final changes

--- scripts/data.js
+++ scripts/data.js
@@ -3,14 +3,24 @@ let modal = document.getElementsByClassName("modal")[0];
 let closeButton = document.getElementsByClassName("close_button")[0];
 modalTitle = modal.getElementsByTagName("h2")[0];
 
+modalDescription = modal.getElementsByClassName("modal_description")[0];
+modalImage = modal.getElementsByClassName("modal_body").item(0).getElementsByTagName("img")[0];
+modalDate = modal.getElementsByClassName("modal_header").item(0).getElementsByTagName("p")[0];
+modalDuration = modal.getElementsByClassName("modal_header").item(0).getElementsByTagName("span")[1];
+modalRated = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[0];
+modalImdbScore = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[1];
+modalDirectors = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[2];
+modalActors = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[3];
+modalCountries = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[4];
+modalBoxOffice = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[5];
+
+
 
 const moviesApi = "http://localhost:8000/api/v1/titles/?imdb_score_min=8.3&page_size=100&sort_by=-imdb_score";
 
 
 let bestMovieTitle = document.getElementsByClassName("best_movie_description").item(0).getElementsByTagName("h2");
 let bestMovieDescription = document.getElementsByClassName("best_movie_description").item(0).getElementsByTagName("p");
-console.log("BEST MOVIE DESCRIPTION UN JOUR PEUT ETRE ??????");
-console.log(bestMovieDescription);
 let bestMovieImage = document.getElementsByClassName("best_movie_image").item(0);
 let bestMovieUrl;
 let bestMovieDetails;
@@ -59,7 +69,6 @@ const horrorMovies = {
     details: horrorMoviesDetails
 }
 
-
 let musicalMoviesCarrousel = document.getElementsByClassName("carrousel_container musical").item(0);
 let musicalMoviesImages;
 let musicalMoviesDetails;
@@ -70,3 +79,4 @@ const musicalMovies = {
     images: musicalMoviesImages,
     details: musicalMoviesDetails
 }
+
