86e7ccc8348ade7689bfec1c4b1005ea0f21e748
Mathieusc <62360534+Mathieusc@users.noreply.github.com>
2021-11-10 01:27:11 +0100

    Dev (#3)
    * refacto fetch calls
    
    * final changes
    
    * refacto, final changes

--- scripts/data.js
+++ scripts/data.js
@@ -1,82 +1,51 @@
-// Modal elements:
-let modal = document.getElementsByClassName("modal")[0];
-let closeButton = document.getElementsByClassName("close_button")[0];
-modalTitle = modal.getElementsByTagName("h2")[0];
-
-modalDescription = modal.getElementsByClassName("modal_description")[0];
-modalImage = modal.getElementsByClassName("modal_body").item(0).getElementsByTagName("img")[0];
-modalDate = modal.getElementsByClassName("modal_header").item(0).getElementsByTagName("p")[0];
-modalDuration = modal.getElementsByClassName("modal_header").item(0).getElementsByTagName("span")[1];
-modalRated = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[0];
-modalImdbScore = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[1];
-modalDirectors = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[2];
-modalActors = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[3];
-modalCountries = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[4];
-modalBoxOffice = modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[5];
-
-
-
+// API Url for each movies, sorted by atleast 8.3 minimum imdb score.
 const moviesApi = "http://localhost:8000/api/v1/titles/?imdb_score_min=8.3&page_size=100&sort_by=-imdb_score";
 
 
-let bestMovieTitle = document.getElementsByClassName("best_movie_description").item(0).getElementsByTagName("h2");
-let bestMovieDescription = document.getElementsByClassName("best_movie_description").item(0).getElementsByTagName("p");
-let bestMovieImage = document.getElementsByClassName("best_movie_image").item(0);
-let bestMovieUrl;
-let bestMovieDetails;
-
-const bestMovie = {
-    url: moviesApi.replace("imdb_score_min=8.3", "imdb_score_min=9.6"),
-    image: bestMovieImage,
-    details: bestMovieDetails,
-    title: bestMovieTitle,
-    description: bestMovieDescription
+// Modal elements :
+let modal = document.getElementsByClassName("modal")[0];
+let closeModalButton = document.getElementsByClassName("close_button")[0];
+let modalElements = {
+    title: modal.getElementsByTagName("h2")[0],
+    description: modal.getElementsByClassName("modal_description")[0],
+    image: modal.getElementsByClassName("modal_body").item(0).getElementsByTagName("img")[0],
+    date: modal.getElementsByClassName("modal_header").item(0).getElementsByTagName("p")[0],
+    duration: modal.getElementsByClassName("modal_header").item(0).getElementsByTagName("span")[1],
+    rated: modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[0],
+    imdbScore: modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[1],
+    directors: modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[2],
+    actors: modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[3],
+    countries: modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[4],
+    boxOffice: modal.getElementsByClassName("modal_footer").item(0).getElementsByTagName("p")[5]
 };
 
 
-let topRatedMoviesCarrousel = document.getElementsByClassName("carrousel_container top-rated").item(0);
-let topRatedMoviesImages;
-let topRatedMoviesDetails;
+// Each movies categories :
+let bestMovie = {
+    url: moviesApi.replace("imdb_score_min=8.3", "imdb_score_min=9.6"),
+    image: document.getElementsByClassName("best_movie_image").item(0),
+    title: document.getElementsByClassName("best_movie_description").item(0).getElementsByTagName("h2"),
+    description: document.getElementsByClassName("best_movie_description").item(0).getElementsByTagName("p"),
+    button: document.getElementsByClassName("best_movie_description").item(0).getElementsByTagName("button")[0]
+};
 
-
-const topRatedMovies = {
+let topRatedMovies = {
     url: moviesApi.replace("imdb_score_min=8.3", "imdb_score_min=9.3"),
-    carrousel: topRatedMoviesCarrousel,
-    images: topRatedMoviesImages,
-    details: topRatedMoviesDetails
-}
+    carrousel: document.getElementsByClassName("carrousel_container top-rated").item(0)
+};
 
-let animeMoviesCarrousel = document.getElementsByClassName("carrousel_container anime").item(0);
-let animeMoviesImages;
-let animeMoviesDetails;
+let animeMovies = {
+    url: `${moviesApi} ${"&genre=Animation"}`,
+    carrousel: document.getElementsByClassName("carrousel_container anime").item(0)
+};
 
-const animeMovies = {
-    url: moviesApi + "&genre=Animation",
-    carrousel: animeMoviesCarrousel,
-    images : animeMoviesImages,
-    details: animeMoviesDetails
-}
+let horrorMovies = {
+    url: `${moviesApi} + ${"&genre=Horror"}`,
+    carrousel: document.getElementsByClassName("carrousel_container horror").item(0)
+};
 
-
-let horrorMoviesCarrousel = document.getElementsByClassName("carrousel_container horror").item(0);
-let horrorMoviesImages;
-let horrorMoviesDetails;
-
-const horrorMovies = {
-    url: moviesApi + "&genre=Horror",
-    carrousel: horrorMoviesCarrousel,
-    images: horrorMoviesImages,
-    details: horrorMoviesDetails
-}
-
-let musicalMoviesCarrousel = document.getElementsByClassName("carrousel_container musical").item(0);
-let musicalMoviesImages;
-let musicalMoviesDetails;
-
-const musicalMovies = {
-    url: moviesApi + "&genre=Musical",
-    carrousel: musicalMoviesCarrousel,
-    images: musicalMoviesImages,
-    details: musicalMoviesDetails
-}
+let musicalMovies = {
+    url: `${moviesApi} + ${"&genre=Musical"}`,
+    carrousel: document.getElementsByClassName("carrousel_container musical").item(0)
+};
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,44 @@
-const fetchBestMovie = async (movie) => {
+/*
+Fetch movies functions
 
+@param : {objects} movies elements from each categories.
+*/
+const fetchBestMovie = async (movie) => {
     movie["details"] = await fetch(movie["url"])
     .then(response => response.json());
+
+    // Dynamic image creation with the corresponding source.
     let bestImage = document.createElement("img");
     bestImage.src = movie["details"].results[0].image_url;
     bestImage.id = movie["details"].results[0].id;
     movie["image"].appendChild(bestImage);
-    bestImage.addEventListener("click", () => openModal(bestImage.id));
-    closeButton.addEventListener("click", closeModal);
+
+    // Open/close modals functions calls.
+    movie["button"].addEventListener("click", () => openModal(bestImage.id));
+    closeModalButton.addEventListener("click", closeModal);
     movie["title"][0].innerHTML = movie["details"].results[0].title;
+
     updateBestMovieDescription(bestImage.id);
-    
+};
+
+const updateBestMovieDescription = async (id) => {
+    let movieDetail = await fetch("http://localhost:8000/api/v1/titles/" + id)
+    .then(response => response.json());
+
+    bestMovie["description"][0].innerHTML = movieDetail.description;
 };
 
 
 const fetchMovies = async (movies) => {
     movies["details"] = await fetch(movies["url"])
     .then(response => response.json());
-    console.log(movies["details"]);
-    
+
+    // Separate the best movie from the rest.
     if (movies == topRatedMovies) {
         movies["details"].results.shift();
     };
-    
+
+    // Dynamically create images inside the carrousel of each movies categories.
     for (let i = 0; i < 7; i ++) {
         let newImage = document.createElement("img");
         newImage = document.createElement("img");
@@ -30,57 +46,54 @@ const fetchMovies = async (movies) => {
         newImage.id = movies["details"].results[i].id;
         movies["carrousel"].appendChild(newImage);
         newImage.addEventListener("click", () => openModal(newImage.id));
-        closeButton.addEventListener("click", closeModal);
+        closeModalButton.addEventListener("click", closeModal);
     };
-
 };
 
 
+/*
+Open Modal function.
+
+@param : {str} ID of each movie from the click event :
+Fetch call the movie ID and add it's details inside the modal.
+*/
 const openModal = async (id) => {
-    let movieDetails;
-    movieDetails = await fetch("http://localhost:8000/api/v1/titles/" + id)
+    let movieDetails = await fetch("http://localhost:8000/api/v1/titles/" + id)
     .then(response => response.json());
-    console.log(movieDetails);
+
     modal.style.display = "block";
 
-    modalTitle.innerHTML = movieDetails.original_title;
-    modalImage.src = movieDetails.image_url;
-    modalDescription.innerHTML = movieDetails.long_description + "<br><br><strong>Actors</strong> : " + movieDetails.actors;
-    modalDate.innerHTML = movieDetails.date_published;
-    // let hours = Math.floor(totalSeconds / 3600);
-    // let totalSeconds;
-    // totalSeconds %= 3600;
-    // let minutes =  Math.floor(totalSeconds / 60);
-    // let seconds = totalSeconds % 60;
-    modalDuration.innerHTML = displayDuration(movieDetails.duration);
-    modalRated.innerHTML = movieDetails.rated;
-    modalImdbScore.innerHTML = movieDetails.imdb_score + "\n&#11088";
-    modalDirectors.innerHTML = "Directors : " + movieDetails.directors;
-    modalActors.innerHTML = movieDetails.genres;
-    modalCountries.innerHTML = movieDetails.countries;
-    modalBoxOffice.innerHTML = "Gross income : " + new Intl.NumberFormat("en-us", { style: "currency", currency: "USD" }).format(movieDetails.worldwide_gross_income);
-
+    // Insert movie details inside the modal.
+    modalElements["title"].innerHTML = movieDetails.original_title;
+    modalElements["image"].src = movieDetails.image_url;
+    modalElements["description"].innerHTML = `${movieDetails.long_description}
+                                              ${"<br><br><strong>Actors</strong> : "}
+                                              ${movieDetails.actors}`;
+    modalElements["date"].innerHTML = movieDetails.date_published;
+    modalElements["duration"].innerHTML = displayDuration(movieDetails.duration);
+    modalElements["rated"].innerHTML = movieDetails.rated;
+    modalElements["imdbScore"].innerHTML = `${movieDetails.imdb_score}
+                                            ${"\n&#11088"}`;
+    modalElements["directors"].innerHTML = `${"Directors : "}
+                                            ${movieDetails.directors}`;
+    modalElements["actors"].innerHTML = movieDetails.genres;
+    modalElements["countries"].innerHTML = movieDetails.countries;
+    modalElements["boxOffice"].innerHTML = `${"Gross income : "} ${new Intl.NumberFormat("en-us", {style: "currency", currency: "USD"})
+                                            .format(movieDetails.worldwide_gross_income)}`;
 };
 
+// Display the movie duration with the 0h0m format.
 const displayDuration = (minutes) => {
     let hours = Math.floor(minutes / 60);
     let minute = minutes % 60;
-    // let seconds = Math.floor(minute / 3600);
-    return hours + "h" + minute + "m";
-}
+    return `${hours}${"h"}${minute}${"m"}`;
+
+};
 
 
 const closeModal = () => {
     modal.style.display = "none";
 };
-
-
-const updateBestMovieDescription = async (id) => {
-    let movieDetail = await fetch("http://localhost:8000/api/v1/titles/" + id)
-    .then(response => response.json());
-    bestMovie["description"][0].innerHTML = movieDetail.description;
-};
-
 
 fetchBestMovie(bestMovie);
 fetchMovies(topRatedMovies);
