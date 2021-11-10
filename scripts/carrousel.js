3b3214ba1baf8523535866a402122b9f63de8a38
Mathieusc <mathieu.schweitzer@hotmail.fr>
2021-11-10 01:16:11 +0100

    refacto, final changes

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,3 +1,4 @@
+// Carrousel elements :
 const topRatedImages = document.getElementsByClassName("carrousel_container top-rated").item(0).getElementsByTagName("img");
 const animeImages = document.getElementsByClassName("carrousel_container anime").item(0).getElementsByTagName("img");
 const horrorImages = document.getElementsByClassName("carrousel_container horror").item(0).getElementsByTagName("img");
@@ -9,8 +10,13 @@ const mapImage = {
                 horror: horrorImages,
                 musical: musicalImages};
 
-                
-// Previous and next slider, each image will be replaced either on the last or the first position of the carrousel.
+
+/*
+Move next/previous sliders functions.
+Swap the position of an image on the last or the first carrousel's spot.
+
+@param: Carrousel of each categories.
+*/
 const movePrevious = (imageType) => {
     const imageList = mapImage[imageType];
     imageList[imageList.length -1].parentNode.insertBefore(imageList[imageList.length -1], imageList[0]);
@@ -23,18 +29,18 @@ const moveNext = (imageType) => {
 };
 
 
-// Top rated movies slide
+// Top rated movies slide :
 document.querySelector(".button_best_left").addEventListener("click", () => movePrevious("topRated"));
 document.querySelector(".button_best_right").addEventListener("click", () => moveNext("topRated"));
 
-// Anime slide
+// Anime slide :
 document.querySelector(".button_anime_left").addEventListener("click", () => movePrevious("anime"));
 document.querySelector(".button_anime_right").addEventListener("click", () => moveNext("anime"));
 
-// Horror movies slide
+// Horror movies slide :
 document.querySelector(".button_horror_left").addEventListener("click", () => movePrevious("horror"));
 document.querySelector(".button_horror_right").addEventListener("click", () => moveNext("horror"));
 
-// Musical movies slide
+// Musical movies slide :
 document.querySelector(".button_musical_left").addEventListener("click", () => movePrevious("musical"));
 document.querySelector(".button_musical_right").addEventListener("click", () => moveNext("musical"));
